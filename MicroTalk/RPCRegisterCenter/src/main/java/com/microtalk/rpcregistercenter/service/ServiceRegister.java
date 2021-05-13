1da03a2527f99d609c3ca3b408c59d7555c49634
UIT2016 <3492770@qq.com>
2021-05-13 17:10:02 +0800

    无用

--- MicroTalk/RPCRegisterCenter/src/main/java/com/microtalk/rpcregistercenter/service/ServiceRegister.java
+++ MicroTalk/RPCRegisterCenter/src/main/java/com/microtalk/rpcregistercenter/service/ServiceRegister.java
@@ -1,4 +1,24 @@
 package com.microtalk.rpcregistercenter.service;
-
+/**
+ *
+ *
+ *
+ * 服务注册接口
+ **/
 public interface ServiceRegister {
+ /**
+ *注册中心初始化
+ * @param
+ * @return
+ *
+ **/
+ void init();
+ /**
+  *服务注册
+  * @param
+  * @return
+  *
+  **/
+void register(String serviceName,String serviceAddress);
+
 }
--- MicroTalk/RPCRegisterCenter/src/main/java/com/microtalk/rpcregistercenter/service/impl/ZkServiceRegister.java
+++ MicroTalk/RPCRegisterCenter/src/main/java/com/microtalk/rpcregistercenter/service/impl/ZkServiceRegister.java
@@ -1,9 +1,33 @@
 package com.microtalk.rpcregistercenter.service.impl;
 
+import com.microtalk.rpcregistercenter.common.constant.ZkConstants;
+import com.microtalk.rpcregistercenter.service.ServiceRegister;
+import jdk.nashorn.internal.runtime.GlobalConstants;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+
 /**
  * @author 812
  * @description: 服务注册
  * @date 2021-05-13 15:08:11
  */
-public class ZkServiceRegister  {
+public class ZkServiceRegister  implements ServiceRegister {
+private ZooKeeper zooKeeper =null;
+
+    @Override
+    public void init() {
+        //todo 以后再添加watcher机制
+        try {
+            zooKeeper=new ZooKeeper(ZkConstants.ZK_HOST,ZkConstants.ZK_SESSIONTIMEOUT,null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void register(String serviceName, String serviceAddress) {
+
+    }
 }
